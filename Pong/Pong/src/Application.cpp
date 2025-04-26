a26302b20c482efdb8cc4af97d59bb55d83053da
ezryth <ezra.mason@outlook.com>
2025-04-26 23:50:01 +0100

    feat: add glad dependencies and update Application.cpp to render a white window using glad

--- Pong/Pong/src/Application.cpp
+++ Pong/Pong/src/Application.cpp
@@ -1,29 +1,73 @@
-#include <iostream>
+#include <glad/glad.h> 
 #include <GLFW/glfw3.h>
+#include <iostream>
 
-int main() {
-	GLFWwindow* Window;
-	if (!glfwInit())
-	{
-		return -1;
-	}
+/// <summary>
+/// Callback for GLFW when the window size is updated
+/// </summary>
+/// <param name="window"></param>
+/// <param name="width"></param>
+/// <param name="height"></param>
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
+/// <summary>
+/// Handle inputs through GLFW
+/// </summary>
+/// <param name="window"></param>
+void ProcessInput(GLFWwindow* window);
 
-	Window = glfwCreateWindow(640, 480, "Hello Window", NULL, NULL);
+const int c_ScreenWidth = 640;
+const int c_ScreenHeight = 480;
 
-	if (!Window)
-	{
-		glfwTerminate();
-		return -1;
-	}
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	glfwMakeContextCurrent(Window);
+    GLFWwindow* window = glfwCreateWindow(c_ScreenWidth, c_ScreenHeight, "Pong", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
 
-	while (!glfwWindowShouldClose(Window)) {
-		glClear(GL_COLOR_BUFFER_BIT);
-		glfwSwapBuffers(Window);
-		glfwPollEvents();
-	}
+    glfwMakeContextCurrent(window);
 
-	glfwTerminate();
-	return 0;
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialise GLAD" << std::endl;
+        return -1;
+    }
+
+    glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        ProcessInput(window);
+
+        glClearColor(1.f, 1.f, 1.f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
+
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void ProcessInput(GLFWwindow* window) 
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
 }

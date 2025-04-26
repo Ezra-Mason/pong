55aad6b28039b4d3947d925dfc4e8616b7010250
EZ-LAPTOP\ezram <ezra.mason@outlook.com>
2025-04-26 21:02:15 +0100

    feat: add GLFW dependencies and update Application.cpp to create blank window

--- Pong/Pong/src/Application.cpp
+++ Pong/Pong/src/Application.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
-int main() 
-{
-	std::cout << "Hello World" << std::endl;
+int main() {
+	GLFWwindow* Window;
+	if (!glfwInit())
+	{
+		return -1;
+	}
+
+	Window = glfwCreateWindow(640, 480, "Hello Window", NULL, NULL);
+
+	if (!Window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(Window);
+
+	while (!glfwWindowShouldClose(Window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(Window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }

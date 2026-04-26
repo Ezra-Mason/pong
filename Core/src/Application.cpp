66bbf7f00c58ce9b5f34c86a2c18760cf405be59
EZ-LAPTOP\ezram <ezra.mason@outlook.com>
2026-04-26 22:58:32 +0100

    feat: add colour data to vertices

--- Core/src/Application.cpp
+++ Core/src/Application.cpp
@@ -81,10 +81,10 @@ int Application::Run()
 	glDeleteShader(fragmentShaderId);
 
 	float vertices[] = {
-		 0.5f,  0.5f, 0.0f,  // top right
-		 0.5f, -0.5f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  // bottom left
-		-0.5f,  0.5f, 0.0f   // top left 
+		 0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // top right
+		 0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f,// bottom right
+		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,// bottom left
+		-0.5f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f// top left 
 	};
 
 	unsigned int indices[] = {  // note that we start from 0!
@@ -109,8 +109,10 @@ int Application::Run()
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// link vertex attribute pointers
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	// render in wireframe mode
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
